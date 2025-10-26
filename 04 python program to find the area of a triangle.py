e3302208edc9784458e2e2609e616892c2964842
Ujjval-crypto <Ujjvalchaturvedi14@gmail.com>
2025-10-26 20:58:55 +0530

    Add files via upload

---
+++ 04 python program to find the area of a triangle.py
@@ -0,0 +1,9 @@
+# Taking base and height:
+base_of_triangle = int(input("Enter the base: "))
+height_of_triangle = int(input("Enter the height: "))
+
+# calculating the area of triangle
+area_of_triangle = 1/2 * base_of_triangle * height_of_triangle
+
+# printing the area of triangle
+print(area_of_triangle)
